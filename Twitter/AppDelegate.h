220a6a2073175db18f8cae1a97e1a27160e49740
sindsri <sindhujas.psgtech@gmail.com>
2014-10-06 11:53:11 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Sindhuja Sridharan on 10/6/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
